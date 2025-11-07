32cddde7682909349f37b8c25d1c3f1e9803b45e
cho58 <oke08034@snu.ac.kr>
2025-11-08 05:30:22 +0900

    kakao error

--- config/settings.py
+++ config/settings.py
@@ -169,7 +169,6 @@ REST_FRAMEWORK = {
 }
 
 # Kakao OAuth 설정
-KAKAO_REST_API_KEY = env('KAKAO_REST_API_KEY', default='your-kakao-rest-api-key')
 KAKAO_REDIRECT_URI = env('KAKAO_REDIRECT_URI', default='http://localhost:8000/api/user/kakao/callback/')
 KAKAO_CLIENT_SECRET = env('KAKAO_CLIENT_SECRET', default='your-kakao-client-secret')
 
