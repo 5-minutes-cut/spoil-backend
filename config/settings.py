ecce328b5a8d90a1089a46a83981663aea5bf34b
sisihae <sihyunkim@snu.ac.kr>
2025-11-08 05:42:14 +0900

    add allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,7 @@ OPENAI_API_KEY = env('OPENAI_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'spoil-backend.onrender.com']
 
 
 # Application definition
