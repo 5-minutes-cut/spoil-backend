efda8ea5dc21b408a86b93f7d1582ea069ee6bd5
sisihae <sihyunkim@snu.ac.kr>
2025-11-08 05:11:27 +0900

    fix

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'user',
     'rest_framework_simplejwt.token_blacklist',
     'rest_framework',
     'corsheaders',
