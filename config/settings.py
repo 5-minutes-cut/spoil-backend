0547d43dad197559ec82fac51488e7a6e1b215d2
sisihae <sihyunkim@snu.ac.kr>
2026-01-18 17:13:27 +0900

    deploy: add secrets needed for ci check

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,11 @@ KAKAO_REST_API_KEY = env('KAKAO_SECRET_KEY')
 KAKAO_REDIRECT_URI = env('KAKAO_REDIRECT_URI', default='http://localhost:8000/api/user/kakao/callback/')
 KAKAO_CLIENT_SECRET = env('KAKAO_CLIENT_SECRET', default='')
 
+# Channel.io API 설정
+CHANNEL_OPEN_ACCESS_KEY = env('CHANNEL_OPEN_ACCESS_KEY', default='')
+CHANNEL_IO_ACCESS_SECRET = env('CHANNEL_IO_ACCESS_SECRET', default='')
+CHANNEL_OPEN_BASE_URL = env('CHANNEL_OPEN_BASE_URL', default='https://api.channel.io/open/v5')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
